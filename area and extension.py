089fec0ce31bf2316de45a0dab48404fa319394d
Rapellikeerthi <79577690+Rapellikeerthi@users.noreply.github.com>
2021-11-13 21:56:02 +0530

    Create area and extension.py

---
+++ area and extension.py
@@ -0,0 +1,13 @@
+#Area of circle
+
+PI=3.14
+r=float(input("Enter the radius of the circle:"))
+area=PI*r*r
+print("area of the circle =%f" %area)
+
+
+
+#The extension of the file
+filename=input("Input the Filename:")
+f_extns=filename.split(".")
+print("The extension of the file is:"+repr(f_extns[-1]))
