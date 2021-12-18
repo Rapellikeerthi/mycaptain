2fc0b78a295fe904dea58dc80180a970d66899e8
Rapellikeerthi <79577690+Rapellikeerthi@users.noreply.github.com>
2021-12-19 00:06:21 +0530

    Create web scraping.py

---
+++ web scraping.py
@@ -0,0 +1,24 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+movie_records[]
+movie_page = "https://www.imdb.com/search/title/?groups=top_250&sort=user_rating"
+
+movie_page_html = requests.get(movie_page, headers = {"Accept-Language": "en-US"})
+movie_soup = BeautifulSoup(movie_page_html.content, 'html.parser')
+filtered = movie_soup.find_all('div', {"class": "lister-item mode-advanced"})
+for i in filtered:
+    title = i.find('h3')
+    name = title.find('a').text
+    year = title.find('span', {"class": "lister-item-year"}).text
+
+    rating_bar = i.find('div', {"class": "ratings-imdb-rating"})
+    rating =  rating_bar.find('strong').text
+    
+    movie_records.append({'Movie_Name': name, 'Year':year, 'Ratings':rating})
+
+movie_df = pd.DataFrame(movie_records) 
+movie_df.head()
+movie_df.to_excel("E:/movie_data.xlsx")
+
+
